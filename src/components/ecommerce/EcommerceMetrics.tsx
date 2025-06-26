cedd47bb69ab0abfdd156759d2ee86caa755230e
Muthyalanayudu-Maddu <muthyalanayudu.marketwings@gmail.com>
2025-06-26 10:20:31 +0530

    added

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -81,7 +81,7 @@ export default function EcommerceMetrics() {
 
   return (
     <>
-      <div className="space-y-6">
+      <div className="w-max space-y-6">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 md:gap-6">
           {counts.map((count, index) => (
             <div
