859c54e7988e8b05e998bbc965f2d548a30ebff7
srikrishnameet <srikrishna@meetowner.in>
2025-07-16 12:16:55 +0530

    added the latest changes

--- src/hooks/axiosInstance.ts
+++ src/hooks/axiosInstance.ts
@@ -1,11 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 
 const ngrokAxiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://658c5d9c9561.ngrok-free.app',
-
+  baseURL: 'https://api.mntechs.com',
   headers: { 
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': 'true',
   },
 });
 
