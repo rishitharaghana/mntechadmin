e3d25ae7d89a67107b0884cae2dd5d496301b8b6
rishitharaghana <jrishitha2001@gmail.com>
2025-07-18 12:38:52 +0530

    instance

--- src/hooks/axiosInstance.ts
+++ src/hooks/axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 
 const ngrokAxiosInstance: AxiosInstance = axios.create({
-    // baseURL: 'https://api.mntechs.com',
+     baseURL: 'https://api.mntechs.com',
 
- baseURL: 'http://localhost:4001',
+
 
   headers: { 
     'Content-Type': 'application/json'
